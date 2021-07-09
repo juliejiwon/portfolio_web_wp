7e215be065eec28c0a856dab3b9c731885e42549
julie <juliejiwon@gmail.com>
2021-07-09 18:54:39 +0900

    add projects

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
     {
         filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, '../dist'),
-        publicPath: "/portfolio_web_wp/"
+        // publicPath: "/portfolio_web_wp/"
     },
     devtool: 'source-map',
     plugins:
--- src/script.js
+++ src/script.js
@@ -7,35 +7,47 @@ import { loadImage } from './image'
 import * as $ from 'jquery'
 import { gsap } from 'gsap'
 
-import project1 from '/static/images/enlight_1.jpg'
-import loeyb1 from '/static/images/loeyb_1.png'
+import enlight from '/static/images/enlight_1.jpg'
+import enlight2 from '/static/images/enlight_2.png'
+import loeyb from '/static/images/loeyb_1.png'
+import mego from '/static/images/mego_pc.png'
+import luv from '/static/images/luv.png'
 import loeyb2 from '/static/images/loeyb_2.png'
 import loeyb3 from '/static/images/loeyb_3.png'
+import luv1 from '/static/images/luv_1.png'
+import storyfund from '/static/images/Storyfund.png'
+import AImeetup from '/static/images/AIMeetup_poster.png'
+import ilhwa from '/static/images/ilhwa_1.png'
+import plity from '/static/images/plity.png'
+
 import diamond1 from '/static/texture/diamond1.jpeg'
 
-$('.design, .tech, .business').on('click', function () {
+import profilePic from '/static/images/profile_pic.jpeg'
+
+$('img.loeyb, #loeyb .projectBoxText, img.luv, #luv .projectBoxText, img.mego, #mego .projectBoxText, img.storyfund, #storyfund .projectBoxText, img.enlight, #enlight .projectBoxText, img.plity, #plity .projectBoxText').on('click', function () {
     gsap.to(sphere.position, {
         duration: 1.4,
         z: 1.6,
-        y: -0.5
+        y: 0
     });
     gsap.to(sphere1.position, {
-        duration: 1.4,
+        duration: 1.6,
         z: 1.6,
-        y: -0.5
+        y: 0
     });
+    
 })
 
 $('.about').on('click', function () {
     gsap.to(sphere.position, {
         duration: 1.4,
         z: 1.6,
-        y: 0.5
+        y: 0.45
     });
     gsap.to(sphere1.position, {
         duration: 1.4,
         z: 1.6,
-        y: 0.5
+        y: 0.45
     });
 })
 
@@ -43,10 +55,21 @@ $('.about').on('click', function () {
 const textureLoader = new THREE.TextureLoader()
 // const texture1 = textureLoader.load('/images/grad_8.png')
 const normalTexture = textureLoader.load(diamond1)
-loadImage('img.project1', project1)
-loadImage('img.loeyb1', loeyb1)
+loadImage('img.enlight', enlight)
+loadImage('img.enlight2', enlight2)
+loadImage('img.loeyb', loeyb)
+loadImage('img.mego', mego)
+loadImage('img.luv', luv)
+loadImage('img.plity', plity)
+loadImage('img.plity1', plity)
 loadImage('img.loeyb2', loeyb2)
 loadImage('img.loeyb3', loeyb3)
+loadImage('img.luv1', luv1)
+loadImage('img.AImeetup', AImeetup)
+loadImage('img.storyfund', storyfund)
+loadImage('img.ilhwa', ilhwa)
+
+loadImage('img.profilePic', profilePic)
 
 //color
 const pink1 = new THREE.Color(0xEC82E0)
@@ -67,7 +90,7 @@ const geometry = new THREE.SphereBufferGeometry(0.32, 32, 32)
 const geometry1 = new THREE.SphereBufferGeometry(0.8, 28, 20)
 
 const particlesGeometry = new THREE.BufferGeometry;
-const particlesCnt = 3500;
+const particlesCnt = 4000;
 
 const posArray = new Float32Array(particlesCnt * 3)
 
@@ -91,7 +114,7 @@ const material1 = new THREE.PointsMaterial()
 material1.size = 0.005
 material1.color = lightblue2
 material1.transparent = true
-material1.opacity = .8
+material1.opacity = .68
 
 // material.emissiveMap = texture1
 
@@ -171,8 +194,8 @@ camera.position.z = 2
 scene.add(camera)
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
@@ -188,19 +211,40 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 
+
+// mouse
+document.addEventListener('mousemove', onDocumentMouseMove)
+
+let mouseX=1
+let mouseY=1
+
+let targetX=0
+let targetY=0
+
+const windowHalfX= window.innerWidth/2;
+const windowHalfY = window.innerHeight/2;
+
+function onDocumentMouseMove(event) {
+    mouseX=event.clientX*.01
+    mouseY=event.clientY*.01
+}
 const clock = new THREE.Clock()
 
 const tick = () => {
 
     const elapsedTime = clock.getElapsedTime()
-
+    targetX= mouseX*0.001
+    targetY=mouseY*0.001
     // Update objects
-    sphere.rotation.y = .05 * elapsedTime
-    sphere1.rotation.y = .05 * elapsedTime
+    sphere.rotation.y = elapsedTime* (windowHalfX*0.0001+targetX)
+    sphere1.rotation.y = elapsedTime * (windowHalfX*0.0001+targetX)
     particlesMesh.rotation.y = 0.005 * elapsedTime
     particlesMesh.rotation.x = 0.005 * elapsedTime
 
     // Update Orbital Controls
+    controls.update()
+    sphere.rotation.y+= .5*(targetX-sphere.rotation.y)
+    
     // controls.update()
 
     // Render
@@ -211,3 +255,26 @@ const tick = () => {
 }
 
 tick()
+
+var media = $('video').not("[autoplay='autoplay']");
+
+//for media, you could give your scroll play videos a class if you need to make it more specific. This would play ALL html5 video tags when scrolled too. 
+var tolerancePixel = 80;
+
+function checkMedia(){
+    // Get current browser top and bottom
+    var scrollTop = $(window).scrollTop() + tolerancePixel;
+    var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
+
+    media.each(function(index, el) {
+        var yTopMedia = $(this).offset().top;
+        var yBottomMedia = $(this).height() + yTopMedia;
+
+        if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){ //view explaination in `In brief` section above
+            $(this).get(0).play();
+        } else {
+            $(this).get(0).pause();
+            // $(this).get(0).currentTime = 0;
+        }
+    });
+}
